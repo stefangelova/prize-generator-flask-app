f0b03ab4733221727b5925fafc7943bb9efa0ba6
Ubuntu <stef@worker.uwdzhwprmu4edpn1cgco5ka02f.zx.internal.cloudapp.net>
2020-04-03 16:01:41 +0000

    docker-compose is working

--- frontend/application/__init__.py
+++ frontend/application/__init__.py
@@ -1,9 +1,12 @@
 import os, pymysql
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import getenv
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root: @localhost/proj2db" #str(os.getenv('DATABASE_URI'))
+app.config['SECRET_KEY'] = getenv('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = str(getenv('DATABASE_URI'))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 from application import routes
--- frontend/application/routes.py
+++ frontend/application/routes.py
@@ -2,11 +2,13 @@ from flask import render_template, request, redirect, url_for
 from application import app
 from flask_wtf import FlaskForm
 from wtforms import SubmitField
-
+from application.forms import GenerateForm
+import requests
 
 @app.route('/', methods=['GET', 'POST'])
 def generate():
-    form = SubmitForm()
-    submit = requests.get("http://service3gen:5003").json()["prize"]
-    #db.commit()
-    return render_template ('generate.html', form=form, submit=submit)
+    form = GenerateForm()
+    result = requests.get("http://service3gen:5003").json() #["prize"]
+    #db.session.add()
+    #db.session.commit()
+    return render_template ('generate.html', form=form, result=result)
